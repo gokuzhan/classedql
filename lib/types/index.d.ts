0f95e473f0e645fff641bf01a1f94d7ba654de8b
Gokul <info.gokullan@gmail.com>
2020-08-19 11:26:59 +0530

    build

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -7,10 +7,11 @@ export declare type initConfig = {
     options?: object;
 };
 declare type AConstructorTypeOf<T> = new (...args: any[]) => T;
-export default class ClassedQL {
+export declare class ClassedQL {
     config: initConfig;
     constructor(database: string, username: string, password?: string, options?: initConfig);
     initialize(collections: Record<string, AConstructorTypeOf<Collection>>): Promise<void>;
 }
+export default ClassedQL;
 export * from './datatypes/datatypes';
 export * from './collection/collection';
