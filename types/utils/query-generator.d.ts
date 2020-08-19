6082fb97c68e1b6c48ce7247d2316d47c7006ce5
Gokul <info.gokullan@gmail.com>
2020-08-19 23:09:14 +0530

    ddl fix

--- types/utils/query-generator.d.ts
+++ types/utils/query-generator.d.ts
@@ -3,6 +3,6 @@ export declare class QueryGenerator {
     create: (database: string, table: string, props: string[]) => string;
     drop: (database: string, table: string) => string;
     alter: (database: string, table: string, props: string[]) => string;
-    table(database: string, table: string, ifNotExists?: boolean): string;
+    table(database: string, table: string, ifExists?: boolean, notExists?: boolean): string;
     column(name: string, config: DocumentPropertyType): string;
 }
